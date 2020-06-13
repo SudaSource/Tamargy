2d2db22983c760d7f26d5ca93b40c11f70ef9130
ibrahim yassin <ibrahim00133@outlook.com>
2020-06-13 12:18:15 +0200

    update code #1

--- Core.EF/Migrations/Configuration.cs
+++ Core.EF/Migrations/Configuration.cs
@@ -18,25 +18,7 @@ namespace Core.EF.Migrations
 
         protected override void Seed(Core.EF.DataCtx.DataCtx db)
         {
-            //db.Users.RemoveRange(db.Users.ToList());
-            db.Likes.RemoveRange(db.Likes.ToList());
-            db.Posts.RemoveRange(db.Posts.ToList());
-            db.Comments.RemoveRange(db.Comments.ToList());
-            db.ErrorLogs.RemoveRange(db.ErrorLogs.ToList());
-
-            //var admin = db.Users.Add(new User
-            //{
-            //    Id = 1,
-            //    Name = "Admin",
-            //    CreateDate = DateTime.Now,
-            //    Gender = Gender.Male,
-            //    IsActive = true,
-            //    SetNewPassword = true,
-            //    Phone = "0965438114",
-            //    Role = Role.Admin,
-            //    Serial = Guid.NewGuid().ToString(),
-            //    Email = "admin@sudasource.com"
-            //});
+           
         }
     }
 }
